f38caab31c37e39a251e5782c8589f66b9dd3797
Shimpei Wakida <[shimpei.wakida@gmail.com>
2022-01-30 17:32:26 +0900

    ロジックのリファクタリング

--- ts/EventListener.ts
+++ ts/EventListener.ts
@@ -1,31 +1,38 @@
+import { v4 as uuid } from "uuid";
+
 type Listeners = {
   [id: string]: {
-    event: string
-    element: HTMLElement
-    handler: (e: Event) => void
-  }
-}
+    event: string;
+    element: HTMLElement;
+    handler: (e: Event) => void;
+  };
+};
 
 export class EventListener {
-  private readonly listeners: Listeners = {}
+  private readonly listeners: Listeners = {};
 
-  add(listenerId: string, event: string, element: HTMLElement, handler: (e: Event) => void) {
+  add(
+    event: string,
+    element: HTMLElement,
+    handler: (e: Event) => void,
+    listenerId = uuid()
+  ) {
     this.listeners[listenerId] = {
       event,
       element,
       handler,
-    }
+    };
 
-    element.addEventListener(event, handler)
+    element.addEventListener(event, handler);
   }
 
   remove(listenerId: string) {
-    const listener = this.listeners[listenerId]
+    const listener = this.listeners[listenerId];
 
-    if (!listener) return 
-    
-    listener.element.removeEventListener(listener.event, listener.handler)
+    if (!listener) return;
 
-    delete this.listeners[listenerId]
+    listener.element.removeEventListener(listener.event, listener.handler);
+
+    delete this.listeners[listenerId];
   }
 }
--- ts/index.ts
+++ ts/index.ts
@@ -20,20 +20,17 @@ class Application {
     ) as HTMLElement;
 
     taskItems.forEach(({ task, deleteButtonEl }) => {
-      this.eventListener.add(task.id, "click", deleteButtonEl, () =>
-        this.handleClickDeleteTask(task)
+      this.eventListener.add(
+        "click",
+        deleteButtonEl,
+        () => this.handleClickDeleteTask(task),
+        task.id
       );
     });
 
-    this.eventListener.add(
-      "submit-handler",
-      "submit",
-      createForm,
-      this.handleSubmit
-    );
+    this.eventListener.add("submit", createForm, this.handleSubmit);
 
     this.eventListener.add(
-      "submit-handler",
       "click",
       deleteAllDoneTaskButton,
       this.handleClickDeleteAllTasks
@@ -85,8 +82,11 @@ class Application {
 
     const { deleteButtonEl } = this.taskRenderer.append(task);
 
-    this.eventListener.add(task.id, "click", deleteButtonEl, () =>
-      this.handleClickDeleteTask(task)
+    this.eventListener.add(
+      "click",
+      deleteButtonEl,
+      () => this.handleClickDeleteTask(task),
+      task.id
     );
     titleInput.value = "";
   };
